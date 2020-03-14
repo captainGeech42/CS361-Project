5542455d2c690aee4522c2621b80d964fcce5e8d
Brendan Moore <moorbren@oregonstate.edu>
2020-03-13 20:57:00 -0700

    added upload and logout buttons

--- public/index.js
+++ public/index.js
@@ -13,14 +13,20 @@ function clearCookies(){
     document.cookie = 'user= ; expires = Thu, 01 Jan 1970 00:00:00 GMT';
 }
 
+function logout(){
+    clearCookies();
+    
+    window.location.href = "/my-account"
+}
+
 var userpass = getCookie('user');
 if(userpass){
-    var accountButton = document.querySelector('#account-button');
-    accountButton.classList.add('hidden');
-
+    document.querySelector('#account-button').classList.add('hidden');
     document.querySelector('#create-account-button').classList.add('hidden');
 
+    
     document.querySelector('#account-button2').classList.remove('hidden');
+    document.querySelector('#upload-stuff').classList.remove('hidden');
 }
 
 async function checkIfUserLoggedIn() {
